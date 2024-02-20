8eca388088e7b645bdaefc59190a699042c92972
dev-braga <braga.trader12@gmail.com>
2024-02-20 16:11:01 -0300

    Adicionado menu hb

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+function fecharMenu() {
+    var navBar = document.querySelector('.navbar-collapse');
+    if (navBar.classList.contains('show')) {
+        navBar.classList.remove('show'); 
+    }
+
+}
+
