ddfe6f66d81a6ade481e8c5b80140690400806d4
dev-braga <braga.trader12@gmail.com>
2024-02-20 22:11:47 -0300

    muitas alteracoes feitas

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 
 function fecharMenu() {
     var navBar = document.querySelector('.navbar-collapse');
+    var navbar_toggler = document.querySelector('.navbar-toggler')
+    if(!navbar_toggler.classList.contains('collapsed')){
+        navbar_toggler.classList.add('collapsed')
+    }
     if (navBar.classList.contains('show')) {
-        navBar.classList.remove('show'); 
+        navBar.classList.remove('show');
+
     }
 
 }
