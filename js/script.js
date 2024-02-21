330e3a58eac377d5007b018fd46e7eeff813e76e
dev-braga <braga.trader12@gmail.com>
2024-02-21 02:20:24 -0300

    Animacoes inseridas

--- js/script.js
+++ js/script.js
@@ -11,4 +11,36 @@ function fecharMenu() {
     }
 
 }
+// Funcao de carregamento de imagens
+function loading(){
+    document.getElementsByClassName('box-loading')[0].style.display = 'none'
+    document.getElementsByClassName('content')[0].style.display = 'block'
+}
 
+// uso de rolagem suave
+const lenis = new Lenis()
+lenis.on('scroll', (e) => {
+  
+})
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
+
+
+
+
+// Animacao texto Servicos
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach( (entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show-animation')
+        }else{
+            entry.target.classList.remove('show-animation')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden-animation')
+elements.forEach((element) => myObserver.observe(element))
